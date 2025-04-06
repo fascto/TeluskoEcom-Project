a21f37a5963fb14f811830a19255e6bcf9f40b12
fascto <fascto.05@gmail.com>
2025-04-06 00:09:21 -0300

    Project Initializing

---
+++ src/main/java/com/fafi/teluskoecomproject/TeluskoEcomProjectApplication.java
@@ -0,0 +1,13 @@
+package com.fafi.teluskoecomproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeluskoEcomProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeluskoEcomProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fafi/teluskoecomproject/TeluskoEcomProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fafi.teluskoecomproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeluskoEcomProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
